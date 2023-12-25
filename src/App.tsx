44ca9d20c724ee1adb6c802b6e096924704157f6
yujhenchen <yujhen.chen93@gmail.com>
2023-12-25 21:04:08 +0800

    add text shadow, use grid layout

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,14 @@ function App() {
   // const [count, setCount] = useState(0);
 
   return (
-    <main className="w-full h-screen bg-no-repeat bg-cover bg-center bg-fixed	bg-#82C3EC"></main>
+    <main className="w-full h-screen bg-no-repeat bg-cover bg-center bg-fixed	bg-#82C3EC color-white grid grid-cols-3">
+      <p className="col-span-2">Jen Chen</p>
+      <div className="justify-self-end">Weather</div>
+      <p className="col-span-3 justify-self-center">Current time</p>
+      <div className="self-end">Location</div>
+      <div className="justify-self-center self-center">Main focus</div>
+      <div className="self-end justify-self-end">Todo</div>
+    </main>
   );
 }
 
