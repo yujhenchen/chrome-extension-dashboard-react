39a57b1a12c11abf3731fcfe0a8d2daf11e5fbcf
yujhenchen <yujhen.chen93@gmail.com>
2023-12-26 20:04:35 +0800

    use open weaher api

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,30 @@ import { useEffect, useState } from "react";
 // import viteLogo from "/vite.svg";
 import "./App.css";
 import { getFormattedDateTime, getGeolocationCoordinates } from "./helper";
+import { GEOCoordinates } from "./types";
 
 function App() {
   const [count, setCount] = useState<number>(0);
   const [currentDateTime, setCurrentDateTime] = useState<string>(
     getFormattedDateTime(new Date())
   );
+  const [geoCoordinates, setGeoCoordinates] = useState<GEOCoordinates | null>(
+    null
+  );
+
+  useEffect(() => {
+    if (!geoCoordinates) return;
+    fetch(
+      `${import.meta.env.VITE_OPENWEATHERMAP_URL}?lat=${
+        geoCoordinates.lat
+      }&lon=${geoCoordinates.long}&appid=${
+        import.meta.env.VITE_OPENWEATHERMAP_API_KEY
+      }`
+    )
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  }, [geoCoordinates]);
 
   useEffect(() => {
     const interval = setInterval(countAndGetCurrentDate, 1000);
@@ -21,13 +39,13 @@ function App() {
   }
 
   function getLocation() {
-    getGeolocationCoordinates().then((data) => console.log(data));
+    getGeolocationCoordinates().then((data) => setGeoCoordinates(data));
   }
 
   return (
     <main className="w-full h-screen bg-no-repeat bg-cover bg-center bg-fixed	bg-#82C3EC color-white grid grid-cols-3">
       <p className="col-span-2">Jen Chen</p>
-      <div className="justify-self-end">Weather</div>
+      <div className="justify-self-end">Weather and City</div>
       <h1 className="col-span-3 justify-self-center text-3xl md:text-6xl lg:text-8xl">
         {currentDateTime}
       </h1>
