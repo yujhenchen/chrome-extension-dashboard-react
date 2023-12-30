a4d019dfdec7353b5a64a40975d557c9dc9b2aa7
yujhenchen <yujhen.chen93@gmail.com>
2023-12-30 14:28:34 +0800

    add custom font, clean up icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-// import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
 import "./App.css";
 import { getFormattedDateTime, getGeolocationCoordinates } from "./helper";
--- uno.config.ts
+++ uno.config.ts
@@ -1,6 +1,19 @@
 // uno.config.ts
 import { defineConfig } from 'unocss'
+import presetWebFonts from '@unocss/preset-web-fonts'
+import presetUno from '@unocss/preset-uno'
+
 
 export default defineConfig({
     // ...UnoCSS options
+    presets: [
+        presetUno(),
+        presetWebFonts({
+            provider: 'google',
+            fonts: {
+                sans: 'Roboto',
+                mono: ['Fira Code', 'Fira Mono:400,700'],
+            },
+        }),
+    ],
 })
